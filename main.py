9f3cb33f79eb2507916542f6b8abeee8e30c31e3
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2024-12-26 00:35:24 -0500

    Refactor main.py to introduce initial profit model and enhance final profit calculations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from minizinc import Instance, Model, Solver
 from utils.input_handler import read_input
+import io
 
 data = read_input("input.txt")
 
@@ -11,21 +12,59 @@ population_segment = data["population_segment"]
 business_environment = data["business_environment"]
 num_programs = data["num_programs"]
 
-programs = Model("models/final_models.mzn")  
+initial_model = Model("models/initial_profit.mzn")  
+final_model = Model("models/final_models.mzn")
 
-gecode = Solver.lookup("gecode")
+initial_solver = Solver.lookup("gecode")
+final_solver = Solver.lookup("gecode")
 
-instance = Instance(gecode, programs)
+initial_instance = Instance(initial_solver, initial_model)
+
+initial_instance["num_of_established"] = num_of_established
+initial_instance["x_coordinates"] = x_coordinates
+initial_instance["y_coordinates"] = y_coordinates
+initial_instance["n"] = n
+initial_instance["population_segment"] = population_segment
+initial_instance["business_environment"] = business_environment
+initial_instance["num_programs"] = num_programs
+
+initial_result = initial_instance.solve()
+
+print(initial_result, end='')
+
+for ejecucion in range(num_programs):
+    final_instance = Instance(final_solver, final_model)
+
+    final_instance["num_of_established"] = num_of_established
+    final_instance["x_coordinates"] = x_coordinates
+    final_instance["y_coordinates"] = y_coordinates
+    final_instance["n"] = n
+    final_instance["population_segment"] = population_segment
+    final_instance["business_environment"] = business_environment
+    final_instance["num_programs"] = num_programs
+
+    final_result = final_instance.solve()
+
+    temp_output = io.StringIO()
+    temp_output.write(str(final_result))
+
+    temp_output.seek(0)
+    temp_lines = temp_output.readlines()
+
+    last_line = temp_lines.pop(-1)
+    values = last_line.split()
+    if len(values) == 2:
+        value1, value2 = values
+
+    temp_output.close()
+
+    x_coordinates.extend([int(value1)])
+    y_coordinates.extend([int(value2)])
+    num_of_established += 1
+
+print(final_result, end='')
 
 
-instance["num_of_established"] = num_of_established
-instance["x_coordinates"] = x_coordinates
-instance["y_coordinates"] = y_coordinates
-instance["n"] = n
-instance["population_segment"] = population_segment
-instance["business_environment"] = business_environment
-instance["num_programs"] = num_programs
 
-result = instance.solve()
 
-print("Resultado:", result)
+
